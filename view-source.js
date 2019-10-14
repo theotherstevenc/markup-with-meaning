0411f378236155db215d0d31ac489a375f988a98
theotherstevenc <steven.winston.collins@gmail.com>
2019-10-14 14:11:33 -0400

    temporary view source button
    will workout a better UX/UI

--- view-source.js
+++ view-source.js
@@ -1,6 +1,16 @@
-// ##########################################
-// ### view source, no render ###############
-// ##########################################
+let viewSourceButton = document.createElement(`button`)
+viewSourceButton.setAttribute(`id`, `view-source`)
+viewSourceButton.setAttribute(`style`,
+`
+  position: absolute; right: 0; top: 0;
+  padding: .5em; background: none; border: 1px solid green;
+  font-size: 1em; opacity: .9; z-index:9999; background:white;
+`
+)
+document.body.appendChild(viewSourceButton)
+viewSourceButton.innerHTML = `View Source`
+
+document.getElementById('view-source').addEventListener("click", function() {
 
 let finders = [
   {
@@ -13,6 +23,8 @@ let finders = [
   }
 ];
 
+viewSourceButton.remove()
+
 let markup = document.querySelector(`html`).outerHTML
 for(let finder of finders){
   markup = markup.replace(finder.find, finder.replace);
@@ -22,17 +34,11 @@ let source = document.createElement(`textarea`)
 source.setAttribute(`class`, `source`)
 source.setAttribute(`style`,
 `
-  margin: 0px;
-  height: 100vh;
-  width: 100vw;
-  box-sizing: border-box;
-  position: absolute;
-  top: 0;
-  left: 0;
-  font-size: 1.5em;
-  font-family: monospace;
-  background: #404040;
-  color: #fafafa;`
+  margin: 0px; height: 100vh; width: 100vw;
+  box-sizing: border-box; position: absolute; top: 0; left: 0;
+  font-size: 1.5em; font-family: monospace; background: #404040; color: #fafafa;`
 )
 document.body.appendChild(source)
 source.innerHTML = markup
+
+});
